a7628bba56e22b90f225c526dbafa76d8f321b6d
Rafael <tafacreumrdsantos@gmail.com>
2025-08-14 13:30:23 -0300

    Realocando tudo na pasta public

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas básicas (exemplo)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Porta dinâmica para Render ou 3000 localmente
